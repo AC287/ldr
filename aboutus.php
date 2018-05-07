bb6e7e551c8b40555d4cdecbd4952b046206d4b1
AC287 <arthurchen287@gmail.com>
2018-05-07 13:16:34 -0400

    about page completed.

--- aboutus.php
+++ aboutus.php
@@ -3,12 +3,12 @@
 <?php get_header(); ?>
 <div class="about-overall-cover">
 	<div class="about-imgcover-container">
-		<img src="<?php bloginfo('template_directory')?>/images/banners/about-banner-nyc.jpg">
+		<img src="<?php bloginfo('template_directory')?>/images/banners/about-banner00.jpg">
 	</div>
 	<div class="about-beginning-title">
 		<div class="abt-container">
 			<div class="index-title1">
-				<span>IN THE BEGINNING</span>
+				<span>LDR'S 83 YEAR OLD STORY</span>
 			</div>
 			<div class="index-title1-underline">
 			</div>
@@ -17,256 +17,87 @@
 </div>
 <div class="container">
 	<div class="aboutus-inthebeginning">
-		<div class="aboutus-inthebeginning-img">
-			<img src="<?php bloginfo('template_directory')?>/images/personnel/chief_v1.jpg">
+		<div class="aboutus-inthebeginning-year">
+			<p class="aiy-operatingsince">OPERATING SINCE</p>
+			<p class="aiy-year">1935</p>
 		</div>
 		<div class="aboutus-inthebeginning-contents">
+			<p class="aboutus-content0">About LDR Global Industries</p>
 			<p class="aboutus-content1">
-				In 1947, Abraham “The Chief” Tropper founded Coda Resources in New York City’s Garment District in the Lower East Side of Manhattan. With humble beginnings as a sewing notions supply company, Coda Resources developed the multi-national enterprise that, today, includes Cambridge, LDR, Coda Retail Supply Solutions, and Amram Brands.
-			</p>
-			<p class="aboutus-content1">
-				Abraham Tropper founded Coda Resources upon the simple principle of offering unparalleled service and high quality products. More than 70 years later, his family still operates the business with these values in mind.
+				G&S Supply was formed in Chicago in 1935, locally distributing plumbing products. In 1984 G&S began to manufacture product under the LDR brand and expanded their business by supplying home centers, hardware distributors and mass merchants their plumbing supplies. LDR was then acquired by Coda Resources in 2015.
 			</p>
 		</div>
 	</div>	<!--	end aboutus-inthebeginning  -->
 </div>	<!--  end container  -->
-<!-- <div class="aboutus-timelineimg"> -->
-	<!-- <img src="<?php bloginfo('template_directory')?>/images/timeline.png"> -->
-<!-- </div> -->
 
-<section id="about-timeline">
-  <div class="about-tl-item">
-    <div class="about-tl-bg" ></div>
-    <div class="about-tl-year">
-      <p>1947</p>
-    </div>
-    <div class="about-tl-content">
-      <!-- <h1>Lorem ipsum dolor sit</h1> -->
-      <p>Abraham Tropper opens Coda Resources (called Central Supply) and begins selling notion items in New York City.</p>
-    </div>
-  </div>
-  <div class="about-tl-item">
-    <div class="about-tl-bg" ></div>
-    <div class="about-tl-year">
-      <p>1985</p>
-    </div>
-    <div class="about-tl-content">
-      <!-- <h1 class="about-f3 text-accent ttu">Vestibulum laoreet lorem</h1> -->
-      <p>Coda relocates to its current 100,000 sq. ft. Headquarters in Brooklyn.</p>
-    </div>
-  </div>
-  <div class="about-tl-item">
-    <div class="about-tl-bg" ></div>
-    <div class="about-tl-year">
-      <p>1998</p>
-    </div>
-    <div class="about-tl-content">
-      <!-- <h1 class="about-f3 text-accent ttu">Phasellus mauris elit</h1> -->
-      <p>Cambridge is established.</p>
-    </div>
-  </div>
-  <div class="about-tl-item">
-    <div class="about-tl-bg" ></div>
-    <div class="about-tl-year">
-      <p>2006</p>
-    </div>
-    <div class="about-tl-content">
-      <!-- <h1 class="about-f3 text-accent ttu">Mauris vitae nunc elem</h1> -->
-      <p>Cambridge Shenzhen offices are formed.</p>
-    </div>
-  </div>
-	<div class="about-tl-item">
-		<div class="about-tl-bg" ></div>
-		<div class="about-tl-year">
-			<p>2007-2009</p>
-		</div>
-		<div class="about-tl-content">
-			<!-- <h1 class="about-f3 text-accent ttu">Phasellus mauris elit</h1> -->
-			<p>Cambridge expands product line to include hose clamps, terminals, and wire connectors.</p>
-		</div>
+<div class="about-midsectioncallout">
+	<div class="container">
+		<p>LDR Global Industries prides itself on their expert knowledge of kitchen, bath and plumbing categories. By eating, breathing and dreaming plumbing, LDR is able to manufacture thousands of sku's, offer competitive pricing, customized product assortments, and various plumbing programs.</p>
 	</div>
-	<div class="about-tl-item">
-		<div class="about-tl-bg" ></div>
-		<div class="about-tl-year">
-			<p>2010</p>
-		</div>
-		<div class="about-tl-content">
-			<!-- <h1 class="about-f3 text-accent ttu">Mauris vitae nunc elem</h1> -->
+</div>
 
-			<p> Expands China &amp; Taiwan facilities.</p>
-		</div>
-	</div>
-	<div class="about-tl-item">
-		<div class="about-tl-bg" ></div>
-		<div class="about-tl-year">
-			<p>2013</p>
-		</div>
-		<div class="about-tl-content">
-			<!-- <h1 class="about-f3 text-accent ttu">Phasellus mauris elit</h1> -->
-			<p>Cambridge introduces zipits&trade; Retail product line.</p>
-		</div>
-	</div>
-	<div class="about-tl-item">
-		<div class="about-tl-bg" ></div>
-		<div class="about-tl-year">
-			<p>2015</p>
-		</div>
-		<div class="about-tl-content">
-			<!-- <h1 class="about-f3 text-accent ttu">Mauris vitae nunc elem</h1> -->
-			<p>Cambridge acquires LDR Global industries.</p>
-		</div>
-	</div>
-	<div class="about-tl-item">
-		<div class="about-tl-bg" ></div>
-		<div class="about-tl-year">
-			<p>2016</p>
-		</div>
-		<div class="about-tl-content">
-			<!-- <h1 class="about-f3 text-accent ttu">Phasellus mauris elit</h1> -->
-			<p>Cambridge relocates key operations to Chicago facility.</p>
-		</div>
-	</div>
-</section>
+<div class="about-whyus">
+	<div class="container">
+		<div class="about-whyus-title">
+			<div class="about-whyus-title1">
+				<span>WHY US?</span>
+			</div>
+			<div class="about-whyus-title1-underline">
+			</div>
+		</div> <!-- end about-whyus-title -->
+		<div class="about-whyus-block-container">
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-marketresearch.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>MARKET RESEARCH & CATEGORY EXPERTISE</p>
+				</div>
+			</div>
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-consistentflow.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>CONSISTENT FLOW OF NEW PRODUCTS</p>
+				</div>
+			</div>
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-globalprodev.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>DEDICATED GLOBAL PRODUCT DEVELOPMENT</p>
+				</div>
+			</div>
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-customerservice.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>U.S. BASED CUSTOMER SERVICE AND SALES SUPPORT TEAM</p>
+				</div>
+			</div>
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-qualityprod.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>QUALITY PRODUCTS AT COMPETITIVE PRICE POINT</p>
+				</div>
+			</div>
+			<div class="about-whyus-block-each">
+				<div class="about-whyus-block-img">
+					<img src="<?php bloginfo('template_directory')?>/images/icons/whyus-globaldist.svg">
+				</div>
+				<div class="about-whyus-block-txt">
+					<p>GLOBAL DISTRIBUTION EFFICIENCY</p>
+				</div>
+			</div>
+		</div> <!--  end about-whyus-block-container  -->
+	</div> <!--  end container  -->
+</div>	<!--  end about-whyus  -->
 
-<div class="about-cat about-hvac">
-	<div class="container">
-		<div class="about-cat-img">
-			<img src="<?php bloginfo('template_directory')?>/images/about/about-hvac.jpg">
-		</div>
-		<div class="about-cat-desc">
-			<div class="about-cat-title">
-				HVAC
-			</div>
-			<div class="about-cat-txt">
-				<p class="about-cat-txt-main">
-					Cambridge has been a leading manufacturer of premium HVAC products for almost two decades. Duct and pipe sup- port straps conform to UL standards and have been found to be suitable for use in plenums .
-				</p>
-				<p class="about-cat-txt-tags">
-					Duct Straps, Support Webbing, Gas Connectors, Pipe Hanging, Chip Brushes, and more
-				</p>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="about-cat about-electrical">
-	<div class="container">
-		<div class="about-cat-desc">
-			<div class="about-cat-title">
-				ELECTRICAL
-			</div>
-			<div class="about-cat-txt">
-				<p class="about-cat-txt-main">
-					Cambridge dominates in the electrical industry providing many electrical accessories and electrical product support. In an effort to constantly improve the electrician’s/DIY’er’s experience, Cambridge is always looking to develop and offer better alternatives to the current standards out there.
-				</p>
-				<p class="about-cat-txt-tags">
-					Duct Straps, Support Webbing, Gas Connectors, Pipe Hanging, Chip Brushes, and more
-				</p>
-			</div>
-		</div>
-		<div class="about-cat-img">
-			<img src="<?php bloginfo('template_directory')?>/images/about/about-electrical.jpg">
-		</div>
-	</div>
-</div>
-<div class="about-cat about-plumbing">
-	<div class="container">
-		<div class="about-cat-img">
-			<img src="<?php bloginfo('template_directory')?>/images/about/about-plumbing.jpg">
-		</div>
-		<div class="about-cat-desc">
-			<div class="about-cat-title">
-				PLUMBING
-			</div>
-			<div class="about-cat-txt">
-				<p class="about-cat-txt-main">
-					It may look pretty on the outside but Cambridge wants to make sure it’s running efficiently on the inside. Keep your pipes and air lines supported by using Cambridge’s broad offerings, many of which conform to UL standards.
-				</p>
-				<p class="about-cat-txt-tags">
-					Hose clamps, Quick release clamps, Pipe hanging support, Gas connec- tors, Fittings, Nipples, Pipe, Valves.
-				</p>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="about-cat about-automotive">
-	<div class="container">
-		<div class="about-cat-desc">
-			<div class="about-cat-title">
-				AUTOMOTIVE
-			</div>
-			<div class="about-cat-txt">
-				<p class="about-cat-txt-main">
-					Cambridge is going at full speed to help organize and secure the hoses and pipes in all types of vehicles. Cars, trucks, boats, ATV’s, and more can benefit from the parts that Cambridge offers.
-				</p>
-				<p class="about-cat-txt-tags">
-					Standard Hose Clamps, Fuel Injector clamps, Heavy duty t-bolt clamps, Wire connectors, Terminals, Switches, Wire management, Cable Ties, Electrical Tape, Tools, Support Kits.
-				</p>
-			</div>
-		</div>
-		<div class="about-cat-img">
-			<img src="<?php bloginfo('template_directory')?>/images/about/about-automotive.jpg">
-		</div>
-	</div>
-</div>
-<div class="about-4info">
-	<div class="container">
-		<div class="about-4info-block">
-			<div class="about-4info-img">
-				<img src="<?php bloginfo('template_directory')?>/images/about/about-devprod.png">
-			</div>
-			<div class="about-4info-title">
-				DEVELOPMENT <br/>&amp; PRODUCTION:
-			</div>
-			<div class="about-4info-contents">
-				<p>Product Design</p>
-				<p>Product Development</p>
-				<p>Packaging</p>
-				<p>Marketing</p>
-				<p>Engineering</p>
-				<p>Manufacturing</p>
-				<p>Sourcing</p>
-			</div>
-		</div>
-		<div class="about-4info-block">
-			<div class="about-4info-img">
-				<img src="<?php bloginfo('template_directory')?>/images/about/about-custsales.png">
-			</div>
-			<div class="about-4info-title">
-				CUSTOMER SERVICE <br/>&amp; SALES SUPPORT
-			</div>
-			<div class="about-4info-contents">
-				<p>U.S. based customer service and sales support team.</p>
-			</div>
-		</div>
-		<div class="about-4info-block">
-			<div class="about-4info-img">
-				<img src="<?php bloginfo('template_directory')?>/images/about/about-supplymngt.png">
-			</div>
-			<div class="about-4info-title">
-				SUPPLY CHAIN <br/>MANAGEMENT
-			</div>
-			<div class="about-4info-contents">
-				<p>Dedicated QA/SCM team in Cina oversees all area of quality control and supply chain logistics.
-			</div>
-		</div>
-		<div class="about-4info-block">
-			<div class="about-4info-img">
-				<img src="<?php bloginfo('template_directory')?>/images/about/about-distribution.png">
-			</div>
-			<div class="about-4info-title">
-				STRATEGIC<br/>DISTRIBUTION
-			</div>
-			<div class="about-4info-contents">
-				<p>5 Strategically located Distribution facilities:</p>
-				<p>Brooklyn, NY</p>
-				<p> Simi Valley, CA</p>
-				<p> Houston, TX</p>
-				<p>Chicago, IL</p>
-				<p>Mississuaga, ONT</p>
-			</div>
-		</div>
-	</div>	<!--	end container	-->
-</div>
 <div class="about-locations">
 	<div class="container">
 		<div class="about-locations-title">
